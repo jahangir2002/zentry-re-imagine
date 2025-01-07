ff78c935f0b6b9b68f71e395a1e48ba780a3aa49
jahangir2002 <jahangirshaikh553@gmail.com>
2025-01-07 12:23:46 +0530

    start zentry re-imagine website

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <main>
+      <h1 className='text-5xl text-red-500 font-bold'>Welcom to new project </h1>
+    </main>
+  )
+}
+
+export default App
