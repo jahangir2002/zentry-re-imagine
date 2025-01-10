ed618500e16e2bca44735c25cbdc040d11adb743
jahangir2002 <jahangirshaikh553@gmail.com>
2025-01-10 16:05:34 +0530

    Update Hero component with new video handling, add Button component, and integrate react-icons

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,6 @@
 import { useRef, useState } from 'react'
+import Button from './Button';
+import { TiLocationArrow } from 'react-icons/ti';
 
 const Hero = () => {
 
@@ -30,53 +32,72 @@ const Hero = () => {
 
     const getVideoSrc = (index) => `videos/hero-${index}.mp4`
 
-  return (
-    <div className='relative h-dvh w-screen overflow-x-hidden'>
-        <div id='video-frame' className='relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75 '>
-            <div>
-                <div className='mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg'>
-                    <div onClick={handleMiniVdClick}
-                     className="origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100"
-                     >
-                        <video 
+    return (
+        <div className='relative h-dvh w-screen overflow-x-hidden'>
+            <div id='video-frame' className='relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75 '>
+                <div>
+                    <div className='mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg'>
+                        <div onClick={handleMiniVdClick}
+                            className="origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100"
+                        >
+                            <video
+                                ref={nextVideodRef}
+                                src={getVideoSrc(upcommingVideoIndex)}
+                                loop
+                                muted
+                                id="current-video"
+                                className="size-64 origin-center scale-150 object-cover object-center"
+                                onLoadedData={handleVideoLoad}
+
+                            />
+                        </div>
+                    </div>
+
+                    <video
                         ref={nextVideodRef}
-                        src={getVideoSrc(upcommingVideoIndex)}
+                        src={getVideoSrc(currentIndex)}
                         loop
                         muted
-                        id="current-video"
-                        className="size-64 origin-center scale-150 object-cover object-center"
+                        id='next-vodeo'
+                        className='absolute-center invisible absolute z-10 size-64 object-cover object-center'
                         onLoadedData={handleVideoLoad}
 
-                        />
-                    </div>
+                    />
+
+                    <video
+                        src={getVideoSrc(currentIndex === totalVideos - 1 ? 1 : currentIndex)}
+                        // autoPlay
+                        loop
+                        muted
+                        className='absolute left-0 top-0 size-full object-cover object-center'
+                        onLoadedData={handleVideoLoad}
+                    />
                 </div>
 
-                <video 
-                ref={nextVideodRef}
-                src={getVideoSrc(currentIndex)}
-                loop
-                muted
-                id='next-vodeo'
-                className='absolute-center invisible absolute z-10 size-64 object-cover object-center'
-                onLoadedData={handleVideoLoad}
+                <h1 className='special-font hero-heading absolute bottom-5 right-5 z-40 text-blue-75'>
+                    Gaming
+                </h1>
 
-                />
+                <div className='absolute left-0 top-0 z-40 size-full'>
+                    <div className='mt-24 px-5 sm:px-10'>
+                        <h1 className='special-font hero-heading text-blue-100 '>redefine</h1>
 
-                <video 
-                src={getVideoSrc(currentIndex === totalVideos -1 ? 1 : currentIndex)}
-                autoPlay
-                loop
-                muted
-                className='absolute left-0 top-0 size-full object-cover object-center'
-                onLoadedData={handleVideoLoad}
-                />
+                        <p className='mb-5 max-w-64 font-robert-regular text-blue-100 capitalize'>Enter the metagame <br /> Unleash the Play  Economy </p>
+
+                        <Button id="watch-tralier" title="Watch Tralier"leftIcon={<TiLocationArrow />} containerClass="!bg-yellow-300 flex-center gap-1" />
+
+                    </div>
+
+                </div>
+
             </div>
 
-            <h1 className=''>Gaming</h1>
-             
+            <h1 className='special-font hero-heading absolute bottom-5 right-5 text-black'>
+                    Gaming
+                </h1>
+
         </div>
-    </div>
-  )
+    )
 }
 
 export default Hero
