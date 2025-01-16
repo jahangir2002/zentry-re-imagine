aa44892b1b057608861b1e88e98bf9e81253f93d
jahangir2002 <jahangirshaikh553@gmail.com>
2025-01-16 13:08:53 +0530

    project done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,10 @@
 import { FaGithub, FaGlobe } from "react-icons/fa";
+import { Tooltip } from 'react-tippy';
+import 'react-tippy/dist/tippy.css';
 
 const socialLinks = [
-  { href: "https://zentry.com/", icon: <FaGlobe /> },
-  { href: "https://github.com/jahangir2002/zentry-re-imagine", icon: <FaGithub /> },
+  { href: "https://zentry.com/", icon: <FaGlobe />, tooltip: "Original website of Zentry" },
+  { href: "https://github.com/jahangir2002/zentry-re-imagine", icon: <FaGithub />, tooltip: "Source Code" },
 ];
 
 const Footer = () => {
@@ -15,15 +17,17 @@ const Footer = () => {
 
         <div className="flex justify-center gap-4  md:justify-start">
           {socialLinks.map((link, index) => (
-            <a
-              key={index}
-              href={link.href}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-black transition-colors duration-500 ease-in-out hover:text-white"
-            >
-              {link.icon}
-            </a>
+            <Tooltip title={link.tooltip} position="bottom" trigger="mouseenter">
+              <a
+                key={index}
+                href={link.href}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-black transition-colors duration-500 ease-in-out hover:text-white"
+              >
+                {link.icon}
+              </a>
+            </Tooltip>
           ))}
         </div>
 
