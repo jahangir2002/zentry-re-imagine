e507b77fc78ba86eb021f56b412bf65efd714cae
jahangir2002 <jahangirshaikh553@gmail.com>
2025-01-16 13:02:52 +0530

    project

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import { FaGithub, FaGlobe } from "react-icons/fa";
+
+const socialLinks = [
+  { href: "https://zentry.com/", icon: <FaGlobe /> },
+  { href: "https://github.com/jahangir2002/zentry-re-imagine", icon: <FaGithub /> },
+];
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="w-screen bg-[#5542ff] py-4 text-black">
+      <div className="container mx-auto flex flex-col items-center justify-between gap-4 px-4 md:flex-row">
+        <p className="text-center text-sm font-light md:text-left">
+          ©Nova 2025. All rights reserved
+        </p>
 
-export default Footer
+        <div className="flex justify-center gap-4  md:justify-start">
+          {socialLinks.map((link, index) => (
+            <a
+              key={index}
+              href={link.href}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-black transition-colors duration-500 ease-in-out hover:text-white"
+            >
+              {link.icon}
+            </a>
+          ))}
+        </div>
+
+        <a
+          href="#privacy-policy"
+          className="text-center text-sm font-light hover:underline md:text-right"
+        >
+          Privacy Policy
+        </a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
