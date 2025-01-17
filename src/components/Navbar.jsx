85e83dbe740c23f578c5fa56c002437e80c38517
jahangir2002 <jahangirshaikh553@gmail.com>
2025-01-17 21:49:26 +0530

    Update index.html title and favicon; replace logo image in Navbar with SVG format

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -68,7 +68,7 @@ const Navbar = () => {
                 <nav className="flex size-full items-center justify-between p-4">
                     {/* Logo and Product button */}
                     <div className="flex items-center gap-7">
-                        <img src="/img/logo.png" alt="logo" className="w-10" />
+                        <img src="/logo.svg" alt="logo" className="w-10" />
 
                         <Button
                             id="product-button"
